99f38d62632fcf2864186e3335a149d9a13462b7
Alex Gill <alex@tanku.com>
2023-12-19 10:54:35 +0200

    Initial connection to PaLM from the chatbot Updates to requirements.txt

---
+++ chat/convo.py
@@ -0,0 +1,46 @@
+from google.cloud import aiplatform
+import vertexai
+from vertexai.preview.language_models import TextGenerationModel
+
+
+temperature = 0.1
+vertexai.init(project="tactical-runway-407513", location='us-central1')
+
+parameters = {
+    "temperature": temperature,  # Temperature controls the degree of randomness in token selection.
+    "max_output_tokens": 256,  # Token limit determines the maximum amount of text output.
+    "top_p": 0.8,
+    # Tokens are selected from most probable to least until the sum of their probabilities equals the top_p value.
+    "top_k": 40,  # A top_k of 1 means the selected token is the most probable among all tokens.
+}
+model = TextGenerationModel.from_pretrained("text-bison@001")
+
+
+class Convo:
+    def __init__(self):
+        self._model = model
+        context = parameters.pop('context', None)
+        self._parameters = parameters
+        self._requests = []
+        self._replies = []
+        if context:
+            self._context = context
+        else:
+            self._context = []
+
+    def predict(self, prompt: str):
+        response = self._model.predict(
+            '\n'.join(self._context + [prompt]),
+            **self._parameters,
+        )
+        return response
+
+    def converse(self, prompt: str):
+        self._requests.append(prompt)
+        prompt_parts = self._requests[:1] + self._replies + [prompt]
+        response = self.predict('\n'.join(prompt_parts))
+        self._replies.append(response.text)
+        return response
+
+    def add_context(self, *context_strs):
+        self._context.extend(context_strs)
--- chat/services.py
+++ chat/services.py
@@ -1,22 +1,22 @@
-# chat/services.py
-import logging
 from .models import Message, UserProfile
+from .convo import Convo
 
 
 class AIReplyService:
     @staticmethod
-    def fetch_response_from_llm(receiver):
+    def fetch_response_from_llm(receiver, content: str):
         user = UserProfile.objects.get(id=receiver.id)
-        if user.stock_answer:
-            return user.stock_answer
+        if user.is_bot:
+            c = Convo()
+            return c.predict(content).text
         else:
-            return "Thank you for your message! This is an automatic response."
+            return "This is an automated response. Please set up the LLM to converse"
 
     @staticmethod
     def create_message_and_automatic_response(sender, receiver, content):
         original_message = Message.objects.create(sender=sender, receiver=receiver, content=content)
 
-        automatic_response_content = AIReplyService.fetch_response_from_llm(receiver)
+        automatic_response_content = AIReplyService.fetch_response_from_llm(receiver, content)
         automatic_response = Message.objects.create(
             sender=receiver,
             receiver=sender,
